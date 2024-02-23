a17d19994f628aec84091dd780b3203683f962a1
misha133kv <mihail_fomin_95@mail.ru>
2024-02-23 13:05:09 +0500

    Implemeted search by username

--- src/store/userApi.ts
+++ src/store/userApi.ts
@@ -11,13 +11,26 @@ const initialState: UserState = {
 }
 
 const prepareQuery = (searchString: string): string => {
-  // Разбить строку по запятым и удалить пробелы
-  const ids = searchString.split(',').map((id) => id.trim())
-  // Отфильтровать пустые строки
-  const validIds = ids.filter((id) => id)
-  // Сформировать строку запроса для каждого id
-  const query = validIds.map((id) => `id=${id}`).join('&')
-  // Вернуть сформированный запрос
+  const searchItems = searchString.split(',')
+  // Массив для хранения условий поиска
+  const conditions: string[] = []
+
+  // Проходимся по каждому элементу и проверяем, является ли он числом (id) или строкой (username)
+  searchItems.forEach((item) => {
+    // Убираем пробелы с начала и конца строки
+    const trimmedItem = item.trim()
+    // Если элемент представляет собой число, то добавляем условие поиска по id
+    if (!isNaN(Number(trimmedItem))) {
+      conditions.push(`id=${trimmedItem}`)
+    } else {
+      // Если элемент не является числом, то добавляем условие поиска по username
+      conditions.push(`username=${trimmedItem}`)
+    }
+  })
+
+  // Объединяем все условия поиска с помощью "и" (&)
+  const query = conditions.join('&')
+  // Возвращаем сформированную строку запроса
   return `users?${query}`
 }
 
