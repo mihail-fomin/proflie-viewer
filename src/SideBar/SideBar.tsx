1d3e7fff680640c44779f8b0d517eec4523a59b3
misha133kv <mihail_fomin_95@mail.ru>
2024-02-23 12:36:02 +0500

    Implemented debounce

--- src/SideBar/SideBar.tsx
+++ src/SideBar/SideBar.tsx
@@ -2,16 +2,18 @@ import { useState } from 'react'
 import styles from './SideBar.module.scss'
 import UserList from './UserList/UserList'
 import SearchInput from './SearchInput/SearchInput'
+import { useDebounceValue } from 'usehooks-ts'
 
 const SideBar = () => {
   const [searchQuery, setSearchQuery] = useState<string>('')
+  const [debouncedValue] = useDebounceValue(searchQuery, 500)
 
   return (
     <nav className={styles.sidebar}>
       <h2>Поиск сотрудников</h2>
       <SearchInput value={searchQuery} onChange={setSearchQuery} />
       <h2>Результаты</h2>
-      <UserList searchQuery={searchQuery} />
+      <UserList searchQuery={debouncedValue} />
     </nav>
   )
 }
