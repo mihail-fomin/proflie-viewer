626018aee62301389393f9c6867ed4c5a1e80b1e
misha133kv <mihail_fomin_95@mail.ru>
2024-02-24 14:21:15 +0500

    Added Card layout

--- src/UserProfile/UserProfile.tsx
+++ src/UserProfile/UserProfile.tsx
@@ -1,21 +1,44 @@
 import styles from './UserProfile.module.scss'
 import { useSelector } from 'react-redux'
 import type { RootState } from '../store/index'
+import userImage from '../assets/userImage.svg'
 
 const UserProfile = () => {
   const activeUser = useSelector((state: RootState) => state.user.activeUser)
 
   return (
-    <div className={styles.userProfile}>
+    <section className={styles.wrapper}>
       {activeUser ? (
-        <>
-          <h2>{activeUser.username}</h2>
-          <p>{activeUser.email}</p>
-        </>
+        <div className={styles.userCard}>
+          <img className={styles.avatar} src={userImage} alt="user avatar" />
+          <div className={styles.info}>
+            <h2 className={styles.username}>{activeUser.name}</h2>
+            <p className={styles.email}>
+              <span>email: </span>
+              {activeUser.email}
+            </p>
+            <p className={styles.phone}>
+              <span>phone: </span>
+              {activeUser.phone}
+            </p>
+            <label htmlFor="about">О себе: </label>
+            <p className={styles.about} id="about">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </div>
+        </div>
       ) : (
-        <p>Выберите сотрудника, чтобы посмотреть его профиль</p>
+        <p className={styles.infoMessage}>
+          Выберите сотрудника, чтобы посмотреть его профиль
+        </p>
       )}
-    </div>
+    </section>
   )
 }
 
