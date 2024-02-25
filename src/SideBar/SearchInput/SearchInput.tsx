6f24ecf576b35124c207df3938cf349851ac8025
misha133kv <mihail_fomin_95@mail.ru>
2024-02-25 15:07:51 +0500

    Added invalidity tooltip

--- src/SideBar/SearchInput/SearchInput.tsx
+++ src/SideBar/SearchInput/SearchInput.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
 import { setActiveUser } from '../../store/userApi'
+import { Tooltip } from 'react-tooltip'
+import 'react-tooltip/dist/react-tooltip.css'
 
 interface SearchInputProps {
   value: string
@@ -9,7 +11,7 @@ interface SearchInputProps {
 
 const SearchInput: React.FC<SearchInputProps> = ({ value, onChange }) => {
   const dispatch = useDispatch()
-
+  const [inValid, setInValid] = React.useState<boolean>(false)
   // использовать useEffect, чтобы обновлять конекст при изменении value
   // только после завершения рендеринга компонента SearchInput
   React.useEffect(() => {
@@ -20,22 +22,39 @@ const SearchInput: React.FC<SearchInputProps> = ({ value, onChange }) => {
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const newValue = e.target.value
-    // Валидация: докпускается вводить латинницу, цифры, запятые и пробелы
+    // Валидация: допускается вводить латиницу, цифры, запятые и пробелы
     if (!/^[a-zA-Z0-9, ]*$/.test(newValue)) {
-      // Если введены иные символы, игнорируем изменение
+      // Показываем тултип с подсказкой о некорректных символах
+      setInValid(true)
       return
     }
     // Передаем новое значение обратно в родительский компонент
     onChange(newValue)
+    setInValid(false)
+    // Очищаем тултип при правильном вводе
   }
 
   return (
-    <input
-      type="text"
-      value={value}
-      onChange={handleChange}
-      placeholder="Введите Id или имя"
-    />
+    <>
+      <input
+        type="text"
+        value={value}
+        onChange={handleChange}
+        placeholder="Введите Id или имя"
+        data-tooltip-id="searchInputTooltip"
+      />
+      <Tooltip id="searchInputTooltip" variant="error" isOpen={inValid}>
+        <div>
+          <h3>Некорректные символы.</h3>
+          <h4>Для ввода доступны только:</h4>
+          <ul>
+            <li>• латиница</li>
+            <li>• цифры</li>
+            <li>• запятые или пробелы</li>
+          </ul>
+        </div>
+      </Tooltip>
+    </>
   )
 }
 
