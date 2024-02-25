59f8d62af72fcf7e5129c4ca68dd26286898f826
misha133kv <mihail_fomin_95@mail.ru>
2024-02-25 12:49:27 +0500

    Added NavBar collapse

--- src/SideBar/SideBar.tsx
+++ src/SideBar/SideBar.tsx
@@ -3,13 +3,29 @@ import styles from './SideBar.module.scss'
 import UserList from './UserList/UserList'
 import SearchInput from './SearchInput/SearchInput'
 import { useDebounceValue } from 'usehooks-ts'
+import { ArrowLeftIcon, BarsIcon } from '../utils/Icons.js'
+import classNames from 'classnames'
 
 const SideBar = () => {
   const [searchQuery, setSearchQuery] = useState<string>('')
   const [debouncedValue] = useDebounceValue(searchQuery, 500)
+  const [collapsed, setCollapsed] = useState(false)
 
   return (
-    <nav className={styles.sidebar}>
+    <nav
+      className={classNames(styles.sidebar, {
+        [styles.sidebar_collapsed]: collapsed,
+      })}
+    >
+      <button
+        className={styles.toggleNav}
+        onClick={() => {
+          setCollapsed(!collapsed)
+        }}
+      >
+        {collapsed ? <BarsIcon /> : <ArrowLeftIcon />}
+      </button>
+
       <h2>Поиск сотрудников</h2>
       <SearchInput value={searchQuery} onChange={setSearchQuery} />
       <h2>Результаты</h2>
---
+++ src/utils/Icons.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const BarsIcon: React.FunctionComponent = () => (
+  <i className="fa-solid fa-bars fa-beat"></i>
+)
+
+export const ArrowLeftIcon: React.FunctionComponent = () => (
+  <i className="fa-solid fa-arrow-left fa-beat"></i>
+)
