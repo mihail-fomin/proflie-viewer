0eb1070123ae35594e8195a6104938c589f6a876
misha133kv <mihail_fomin_95@mail.ru>
2024-02-25 15:12:26 +0500

    Added username to header

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import { useSelector } from 'react-redux'
 import styles from './Header.module.scss'
+import { RootState } from '../store'
 
 const Header = () => {
+  const activeUser = useSelector((state: RootState) => state.user.activeUser)
+
   return (
     <header className={styles.header}>
       <h1 className={styles.header__logo}>Жилфонд</h1>
-      <div className={styles.header__user}>Пользователь</div>
+      <div className={styles.header__user}>
+        {activeUser ? activeUser.username : 'Пользователь'}
+      </div>
     </header>
   )
 }
