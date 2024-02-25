00659dfb6fac550d89ab45a8c31d6ba4a4d70add
misha133kv <mihail_fomin_95@mail.ru>
2024-02-25 15:24:25 +0500

    Refactor: extracted prepareQuery

--- src/store/userApi.ts
+++ src/store/userApi.ts
@@ -1,6 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 import { User } from './types'
+import { prepareQuery } from '../utils'
 
 interface UserState {
   activeUser: User | null
@@ -10,31 +11,7 @@ const initialState: UserState = {
   activeUser: null,
 }
 
-const prepareQuery = (searchString: string): string => {
-  const searchItems = searchString.split(',')
-  // Массив для хранения условий поиска
-  const conditions: string[] = []
 
-  // Проходимся по каждому элементу и проверяем, является ли он числом (id) или строкой (username)
-  searchItems.forEach((item) => {
-    // Убираем пробелы с начала и конца строки
-    const trimmedItem = item.trim()
-
-    if (!trimmedItem) return
-    // Если элемент представляет собой число, то добавляем условие поиска по id
-    if (!isNaN(Number(trimmedItem))) {
-      conditions.push(`id=${trimmedItem}`)
-    } else {
-      // Если элемент не является числом, то добавляем условие поиска по username
-      conditions.push(`username=${trimmedItem}`)
-    }
-  })
-
-  // Объединяем все условия поиска с помощью "и" (&)
-  const query = conditions.join('&')
-  // Возвращаем сформированную строку запроса
-  return `users?${query}`
-}
 
 export const userApi = createApi({
   reducerPath: 'userApi',
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -9,3 +9,35 @@ export const processSearchQuery = (query: string): string => {
     ? query.trim().slice(0, -1)
     : query.trim()
 }
+
+/**
+ * Функция для формирования строки запроса, проходит по каждому элементу,
+ * делает необходимые проверки для дальнейшего их объединения.
+ * @param {string} searchString - Строка запроса для обработки.
+ * @returns {string} - Обработанная строка запроса.
+ */
+export const prepareQuery = (searchString: string): string => {
+  const searchItems = searchString.split(',')
+  // Массив для хранения условий поиска
+  const conditions: string[] = []
+
+  // Проходимся по каждому элементу и проверяем, является ли он числом (id) или строкой (username)
+  searchItems.forEach((item) => {
+    // Убираем пробелы с начала и конца строки
+    const trimmedItem = item.trim()
+
+    if (!trimmedItem) return
+    // Если элемент представляет собой число, то добавляем условие поиска по id
+    if (!isNaN(Number(trimmedItem))) {
+      conditions.push(`id=${trimmedItem}`)
+    } else {
+      // Если элемент не является числом, то добавляем условие поиска по username
+      conditions.push(`username=${trimmedItem}`)
+    }
+  })
+
+  // Объединяем все условия поиска
+  const query = conditions.join('&')
+  // Возвращаем сформированную строку запроса
+  return `users?${query}`
+}
